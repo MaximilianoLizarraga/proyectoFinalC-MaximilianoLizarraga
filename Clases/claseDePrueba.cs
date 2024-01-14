da82754f34a6b44c2d70cb939d79b61e3c5cfb99
unknown <maxi.lizarraga.91@gmail.com>
2024-01-14 02:52:28 -0300

    primera entrega

---
+++ Clases/claseDePrueba.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionConsola.Clases
+{
+    public class claseDePrueba
+    {
+        private int id;
+        private string nombreDePrueba;
+        private int numeroRandom;
+
+        public claseDePrueba(int id, string nombreDePrueba, int numeroRandom)
+        {
+            this.Id = id;
+            this.NombreDePrueba = nombreDePrueba;
+            this.NumeroRandom = numeroRandom;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string NombreDePrueba { get => nombreDePrueba; set => nombreDePrueba = value; }
+        public int NumeroRandom { get => numeroRandom; set => numeroRandom = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using AplicacionConsola.Clases;
+
+Usuario usuario1 = new Usuario(12, "Maximiliano", "Lizarraga", "Chapita", 1234,"maxi.lizarraga.91@gmail.com");
+
+Producto producto1 = new Producto(1, "Camiseta", "camiseta de futbol", 30000, 40000, 5, usuario1.Id);
+
+Venta venta1 = new Venta(14, "Felicitaciones! vendiste una camiseta.", usuario1.Id);
+
+ProductoVendido productoVendido1 = new ProductoVendido(1, producto1.Id, 2, venta1.Id);
+
+
+Console.WriteLine("USUARIO:");
+MostrarObjeto(usuario1);
+Console.WriteLine("\n");
+Console.WriteLine("PRODUCTO:");
+MostrarObjeto(producto1);
+Console.WriteLine("\n");
+Console.WriteLine("VENTA:");
+MostrarObjeto(venta1);
+Console.WriteLine("\n");
+Console.WriteLine("PRODUCTO VENDIDO:");
+MostrarObjeto(productoVendido1);
+Console.WriteLine("\n");
+
+
+
+void MostrarObjeto(object objeto)
+{
+    Type tipoObjeto = objeto.GetType();
+    foreach (var propiedad in tipoObjeto.GetProperties())
+    {
+        Console.WriteLine($"{propiedad.Name}: {propiedad.GetValue(objeto)}");
+    }
+}
